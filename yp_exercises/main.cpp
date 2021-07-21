c8f00cef22e529c7c8b8f2a74006d129ead99ef8
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-07-21 18:00:18 +0300

    Initial Commit

---
+++ yp_exercises/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  yp_exercises
+//
+//  Created by Vladimir Andrushchenko on 21.07.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
