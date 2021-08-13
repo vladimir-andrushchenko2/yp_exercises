a743fdc334da8dff7df91f9e0c6dc18deeb62a81
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-08-13 13:15:09 +0300

    house builder passed praktikum

--- HouseBuilder/main.cpp
+++ HouseBuilder/main.cpp
@@ -1,15 +1,45 @@
 #include <cassert>
+#include <stdexcept>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 class House {
-    // Реализуйте самостоятельно
+   public:
+    House(int length, int width, int height) : length_(length), width_(width), height_(height) {}
+
+    int GetLength() const { return length_; }
+
+    int GetHeight() const { return height_; }
+
+    int GetWidth() const { return width_; }
+
+   private:
+    int length_;
+    int width_;
+    int height_;
 };
 
 class Resources {
-    // Реализуйте самостоятельно
+   public:
+    Resources(int brick_count) : brick_count_(brick_count) {}
+
+    void TakeBricks(int count) {
+        if (count > brick_count_ || count < 0) {
+            throw std::out_of_range("attempt to take more bricks than available or negative amount");
+        }
+
+        brick_count_ -= count;
+    }
+
+    int GetBrickCount() const { return brick_count_; }
+
+   private:
+    int brick_count_;
 };
+// При попытке взять отрицательное количество кирпичей либо взять больше кирпичей, чем есть, метод Resources::TakeBricks
+// должен выбросить исключение std::out_of_range
 
 struct HouseSpecification {
     int length = 0;
@@ -19,8 +49,31 @@ struct HouseSpecification {
 // 8 bricks in height * 4 bricks in width = 1 square meter of wall
 
 class Builder {
-    // Реализуйте самостоятельно
+   public:
+    Builder(Resources& resources) : resources_(resources) {}
+
+    // (width + length) * 2 * 4 * height * 8
+    House BuildHouse(const HouseSpecification& spec) {
+        static constexpr int kBricksInRowIsMeter = 4;
+        static constexpr int kBricksInStackIsMeter = 8;
+
+        int perimeter = (spec.width + spec.length) * 2;
+
+        int bricks_required = perimeter * kBricksInRowIsMeter * spec.height * kBricksInStackIsMeter;
+
+        try {
+            resources_.TakeBricks(bricks_required);
+        } catch (const std::out_of_range&) {
+            throw std::runtime_error("builder couldn't access bricks");
+        }
+
+        return {spec.length, spec.width, spec.height};
+    }
+
+   private:
+    Resources& resources_;
 };
+// При нехватке кирпичей на постройку дома Строитель должен выбросить исключение std::runtime_error
 
 int main() {
     Resources resources{10000};
