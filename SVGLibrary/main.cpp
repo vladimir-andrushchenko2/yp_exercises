6f13996e24f50194a2fc4997a458878b736ee189
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-08-14 18:20:16 +0300

    chore

---
+++ SVGLibrary/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SVGLibrary
+//
+//  Created by Vladimir Andrushchenko on 14.08.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
