df08d734dbeb1dd99c58df83a58349d7fa67d318
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-08-16 15:45:07 +0300

    worker observer passed praktikum

--- WorkerHappinessObserver/people.h
+++ WorkerHappinessObserver/people.h
@@ -1,18 +1,21 @@
 #pragma once
 
 #include <string>
+#include <iostream>
+
+using namespace std;
 
 class Person;
 
 // Наблюдатель за состоянием человека.
 class PersonObserver {
-public:
+   public:
     // Этот метод вызывается, когда меняется состояние удовлетворённости человека
-    void OnSatisfactionChanged(Person& /*person*/, int /*old_value*/, int /*new_value*/) {
+    virtual void OnSatisfactionChanged(Person& /*person*/, int /*old_value*/, int /*new_value*/) {
         // Реализация метода базового класса ничего не делает
     }
 
-protected:
+   protected:
     // Класс PersonObserver не предназначен для удаления напрямую
     ~PersonObserver() = default;
 };
@@ -23,96 +26,118 @@ protected:
     связанного с ним наблюдателя
 */
 class Person {
-public:
-    Person(const std::string& name, int age) {
-        // Напишите реализацию самостоятельно
-    }
+   public:
+    Person(const std::string& name, int age) : name_(name), age_(age) {}
 
-    int GetSatisfaction() const {
-        return satisfaction_;
-    }
+    const std::string& GetName() const { return name_; }
 
-    const std::string& GetName() const {
-        return name_;
-    }
+    int GetSatisfaction() const { return satisfaction_; }
 
     // «Привязывает» наблюдателя к человеку. Привязанный наблюдатель
     // уведомляется об изменении уровня удовлетворённости человека
     // Новый наблюдатель заменяет собой ранее привязанного
     // Если передать nullptr в качестве наблюдателя, это эквивалентно отсутствию наблюдателя
-    void SetObserver(PersonObserver* observer) {
-        // Напишите реализацию самостоятельно
-    }
+    void SetObserver(PersonObserver* observer) { observer_ = observer; }
 
-    int GetAge() const {
-        return age_;
-    }
+    int GetAge() const { return age_; }
 
     // Увеличивает на 1 количество походов на танцы
     // Увеличивает удовлетворённость на 1
-    void Dance() {
-        // Напишите тело метода самостоятельно
+    virtual void Dance() {
+        ++dance_count_;
+        SetSatisfaction(GetSatisfaction() + 1);
     }
 
-    int GetDanceCount() const {
-        // Заглушка. Напишите реализацию самостоятельно
-        return 0;
-    }
+    int GetDanceCount() const { return dance_count_; }
 
     // Прожить день. Реализация в базовом классе ничего не делает
-    void LiveADay() {
+    virtual void LiveADay() {
         // Подклассы могут переопределить этот метод
     }
 
-private:
+    virtual ~Person() = default;
+
+   protected:
+    void SetSatisfaction(int value) {
+        if (satisfaction_ != value) {
+            int old_satisfaction = satisfaction_;
+            satisfaction_ = value;
+            // Если у человека есть наблюдатель, сообщаем ему об изменении удовлетворённости
+            if (observer_) {
+                observer_->OnSatisfactionChanged(*this, old_satisfaction, satisfaction_);
+            }
+        }
+    }
+
+   private:
     std::string name_;
     PersonObserver* observer_ = nullptr;
     int satisfaction_ = 100;
-    int age_;
+    int age_{};
+
+    int dance_count_{};
 };
 
 // Рабочий.
 // День рабочего проходит за работой
-class Worker {
-public:
-    Worker(const std::string& name, int age) {
-        // Напишите недостающий код
-    }
+class Worker : public Person {
+   public:
+    Worker(const std::string& name, int age) : Person(name, age) {}
 
     // Рабочий старше 30 лет и младше 40 за танец получает 2 единицы удовлетворённости вместо 1
+    void Dance() override {
+        // Рабочий сперва просто танцует, как обычный человек
+        Person::Dance();
+
+        // Рабочий в возрасте от 30 до 40 во время танца получает
+        // дополнительную порцию удовлетворения
+        if (const int age = GetAge(); age > 30 && age < 40) {
+            SetSatisfaction(GetSatisfaction() + 1);
+        }
+    }
 
     // День рабочего проходит за работой
+    void LiveADay() override {
+        Work();
+    }
 
     // Увеличивает счётчик сделанной работы на 1, уменьшает удовлетворённость на 5
     void Work() {
-        // Напишите тело метода самостоятельно
+        SetSatisfaction(GetSatisfaction() - 5);
+        ++work_counter_;
     }
 
     // Возвращает значение счётчика сделанной работы
     int GetWorkDone() const {
-        // Заглушка. Напишите реализацию самостоятельно
-        return 0;
+        return work_counter_;
     }
+
+   private:
+    int work_counter_{};
 };
 
 // Студент.
 // День студента проходит за учёбой
-class Student {
-public:
-    Student(const std::string& name, int age) {
-        // Напишите недостающий код
-    }
+class Student : public Person {
+   public:
+    Student(const std::string& name, int age) : Person(name, age) {}
 
     // День студента проходит за учёбой
+    void LiveADay() override {
+        Study();
+    }
 
     // Учёба увеличивает уровень знаний на 1, уменьшает уровень удовлетворённости на 3
     void Study() {
-        // Напишите реализацию самостоятельно
+        SetSatisfaction(GetSatisfaction() - 3);
+        ++knowledge_counter_;
     }
 
     // Возвращает уровень знаний
     int GetKnowledgeLevel() const {
-        // Заглушка, напишите реализацию самостоятельно
-        return 0;
+        return knowledge_counter_;
     }
+
+    private:
+    int knowledge_counter_{};
 };
--- WorkerHappinessObserver/supervisor.h
+++ WorkerHappinessObserver/supervisor.h
@@ -3,18 +3,33 @@
 #include "people.h"
 
 /*
-    Надзиратель за уровнем удовлетворённости.
-    Способен наблюдать за состоянием человека
-    Если уровень удовлетворённости человека опустится ниже минимального уровня, Надзиратель
-    побуждает человека танцевать до тех пор, пока уровень уровень удовлетворённости
-    не станет больше или равен максимальному значению
-*/
-class SatisfactionSupervisor {
+ Надзиратель за уровнем удовлетворённости.
+ Способен наблюдать за состоянием человека
+ Если уровень удовлетворённости человека опустится ниже минимального уровня, Надзиратель
+ побуждает человека танцевать до тех пор, пока уровень уровень удовлетворённости
+ не станет больше или равен максимальному значению
+ */
+class SatisfactionSupervisor : public PersonObserver {
 public:
     // Конструктор принимает значение нижнего и верхнего уровня удовлетворённости
-    SatisfactionSupervisor(int min_satisfaction, int max_satisfaction) {
-        // Напишите реализацию самостоятельно
-    }
+    SatisfactionSupervisor(int min_satisfaction, int max_satisfaction)
+    : min_satisfaction_(min_satisfaction), max_satisfaction_(max_satisfaction) {}
 
     // Напишите реализацию самостоятельно
+
+    void OnSatisfactionChanged(Person& p, int old_value, int new_value) override {
+        if (new_value > old_value) {
+            return;
+        }
+
+        if (new_value < min_satisfaction_) {
+            while (p.GetSatisfaction() < max_satisfaction_) {
+                p.Dance();
+            }
+        }
+    }
+
+private:
+    int min_satisfaction_{};
+    int max_satisfaction_{};
 };
