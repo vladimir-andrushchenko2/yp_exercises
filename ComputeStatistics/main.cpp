6a54620ba8d8b74d8a660d00d78717c52f4c8a5a
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-09-20 13:30:41 +0300

    success

--- ComputeStatistics/main.cpp
+++ ComputeStatistics/main.cpp
@@ -4,11 +4,59 @@
 #include <string_view>
 #include <utility>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
-// напишите функцию ComputeStatistics, принимающую 5 параметров:
-// два итератора, выходное значение для суммы, суммы квадратов и максимального элемента
+template <typename InputIt, typename OutSum, typename OutSqSum, typename OutMax>
+void ComputeStatistics(InputIt first, InputIt last, OutSum& out_sum, OutSqSum& out_sq_sum,
+                       OutMax& out_max) {
+    using Elem = std::decay_t<decltype(*first)>;
+
+    constexpr bool need_sum = !is_same_v<OutSum, const nullopt_t>;
+    constexpr bool need_sq_sum = !is_same_v<OutSqSum, const nullopt_t>;
+    constexpr bool need_max = !is_same_v<OutMax, const nullopt_t>;
+
+    if (first == last) {
+        return;
+    }
+
+    Elem output_sum = *first;
+    Elem output_sq_sum;
+
+    if constexpr (need_sq_sum) {
+        output_sq_sum = *first * (*first);
+    }
+
+    Elem output_max = *first;
+
+    for (auto it = first + 1; it != last; ++it) {
+        if constexpr (need_sum) {
+            output_sum += *it;
+        }
+
+        if constexpr (need_max) {
+            output_max = std::max(*it, output_max);
+        }
+
+        if constexpr (need_sq_sum) {
+            output_sq_sum += *it * (*it);
+        }
+    }
+
+    if constexpr (need_sum) {
+        out_sum = output_sum;
+    }
+
+    if constexpr (need_max) {
+        out_max = output_max;
+    }
+
+    if constexpr (need_sq_sum) {
+        out_sq_sum = output_sq_sum;
+    }
+}
+
 
 struct OnlySum {
     int value;
