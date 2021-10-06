5d34b3a392a8f581206a4c3f918190964651fa4e
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-10-06 13:32:16 +0300

    chore

--- IdentityDocument/main.cpp
+++ IdentityDocument/main.cpp
@@ -63,3 +63,66 @@ int main() {
     Passport pass2;
     pass2.PrintUniqueIDCount();
 }
+
+/*
+
+ Test1
+ IdentityDocument::Ctor() : 1
+ Passport::Ctor()
+ Passport::PrintVisa(France) : 1
+ IdentityDocument::Ctor() : 2
+ Passport::Ctor()
+ PrintInfo(3)
+ Passport::PrintID() : 2 expiration date : 6/9/2031
+ IdentityDocument::CCtor() : 3
+ Passport::Dtor()
+ IdentityDocument::Dtor() : 2
+ IdentityDocument::PrintID() : 3
+ IdentityDocument::Dtor() : 3
+ Test2
+ IdentityDocument::Ctor() : 2
+ Passport::Ctor()
+ IdentityDocument::Ctor() : 3
+ DrivingLicence::Ctor()
+ IdentityDocument::Ctor() : 4
+ Passport::Ctor()
+ Passport::PrintID() : 2 expiration date : 6/9/2031
+ DrivingLicence::PrintID() : 3
+ Passport::PrintID() : 4 expiration date : 6/9/2031
+ Passport::Dtor()
+ IdentityDocument::Dtor() : 2
+ DrivingLicence::Dtor()
+ IdentityDocument::Dtor() : 3
+ Passport::Dtor()
+ IdentityDocument::Dtor() : 4
+ Test3
+ IdentityDocument::Ctor() : 2
+ Passport::Ctor()
+ IdentityDocument::CCtor() : 3
+ Passport::Dtor()
+ IdentityDocument::Dtor() : 2
+ IdentityDocument::Ctor() : 3
+ DrivingLicence::Ctor()
+ IdentityDocument::CCtor() : 4
+ DrivingLicence::Dtor()
+ IdentityDocument::Dtor() : 3
+ IdentityDocument::Ctor() : 4
+ IdentityDocument::PrintID() : 3
+ IdentityDocument::PrintID() : 4
+ IdentityDocument::PrintID() : 4
+ IdentityDocument::PrintID() : 3
+ Test4
+ unique_id_count_ : 4
+ Test5
+ IdentityDocument::Ctor() : 5
+ Passport::Ctor()
+ unique_id_count_ : 5
+ Passport::Dtor()
+ IdentityDocument::Dtor() : 5
+ IdentityDocument::Dtor() : 4
+ IdentityDocument::Dtor() : 4
+ IdentityDocument::Dtor() : 3
+ Passport::Dtor()
+ IdentityDocument::Dtor() : 1
+ 
+ */
