27b8d577652bb73d5363e139022c4bbc0d759469
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-10-17 14:52:43 +0300

    about to test

--- LibJpeg/bmp_image.cpp
+++ LibJpeg/bmp_image.cpp
@@ -1,18 +1,18 @@
 #include "bmp_image.h"
-#include "pack_defines.h"
 
 #include <array>
 #include <fstream>
 #include <string_view>
+#include <iostream>
+
+#include "pack_defines.h"
 
 using namespace std;
 
 namespace img_lib {
 
 // функция вычисления отступа по ширине
-static int GetBMPStride(int w) {
-    return 4 * ((w * 3 + 3) / 4);
-}
+static int GetBMPStride(int w) { return 4 * ((w * 3 + 3) / 4); }
 
 PACKED_STRUCT_BEGIN BitmapFileHeader {
     BitmapFileHeader(int width, int height) {
@@ -23,7 +23,7 @@ PACKED_STRUCT_BEGIN BitmapFileHeader {
     char M = 'M';
     uint32_t total_size_of_headers_and_data{};
     uint32_t space_filled_with_zeroes = 0;
-    //size of both headers is 54 bytes
+    // size of both headers is 54 bytes
     uint32_t heades_size = 54;
 }
 PACKED_STRUCT_END
@@ -43,19 +43,41 @@ PACKED_STRUCT_BEGIN BitmapInfoHeader {
     int32_t horizontal_resolution = 11811;
     int32_t vertical_resolution = 11811;
     int32_t colors_used = 0;
+    int32_t meaningful_colors = 0x1000000;
 }
 PACKED_STRUCT_END
 
 // напишите эту функцию
 bool SaveBMP(const Path& file, const Image& image) {
-    ofstream out(file, ios::binary);
     BitmapFileHeader file_header(image.GetWidth(), image.GetHeight());
     BitmapInfoHeader info_header(image.GetWidth(), image.GetHeight());
 
     assert(sizeof(file_header) == 14);
+    // std::cout << sizeof(info_header) << std::endl;
     assert(sizeof(info_header) == 40);
 
-    return false;
+    ofstream out(file, ios::binary);
+
+    out.write(reinterpret_cast<const char*>(&file_header), sizeof(file_header));
+    out.write(reinterpret_cast<const char*>(&info_header), sizeof(info_header));
+
+    const int stride = GetBMPStride(image.GetWidth());
+
+    std::vector<char> buff(stride);
+
+    for (int y = image.GetHeight() - 1; y >= 0; --y) {
+        const Color* line = image.GetLine(y);
+
+        for (int x = 0; x < image.GetWidth(); ++x) {
+            buff[x * 3 + 2] = static_cast<char>(line[x].b);
+            buff[x * 3 + 1] = static_cast<char>(line[x].g);
+            buff[x * 3 + 0] = static_cast<char>(line[x].r);
+        }
+
+        out.write(reinterpret_cast<const char*>(buff.data()), buff.size());
+    }
+
+    return out.good();
 }
 
 // напишите эту функцию
